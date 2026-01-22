c25f3b567df0bb5bdc02f1feb06fe032f4faad55
Dan McGary <dabiggamer13@gmail.com>
2026-01-22 09:11:41 -0700

    adding everything to github

---
+++ daRepo/guessAge.py
@@ -0,0 +1,23 @@
+import random
+
+def main():
+    print("Hello! I'm going to try to guess your name.\n")
+    name = input("First, what is your name? Enter here: ")
+    answer = False
+    guess_list = []
+    while answer == False:
+        guess = random.randint(15, 30)
+        print("Your age is: " + str(guess))
+        clarify = input("Did I get it right? (Type 'y' or 'n'): ")
+        if clarify == 'y':
+            if guess in guess_list:
+                print("Hey! You said no to that earlier! Liar!")
+            answer = True
+        elif clarify == 'n':
+            if guess in guess_list:
+                print("Oh yeah, you said no already. My bad.")
+            answer = False
+            print("Dang! Let me try again...")
+        guess_list.append(guess)
+    print("Yay! I got it! Wanna go again?")
+main()
